b911f8aedd8ee7029ea2c997555d1dca52f149cc
Ildar Usmanov <wajox@mail.ru>
2018-01-25 15:10:35 +0300

    add controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'images/index'
+
+  get 'images/show'
+
+  get 'sourcelinks/index'
+
+  get 'sourcelinks/create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
   
